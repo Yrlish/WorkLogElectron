da640610dfbe7901a546b9822a3690be44147499
Dennis Alexandersson <dennis@alexandersson.xyz>
2020-03-21 21:12:37 +0100

    Updates to title bar

--- app/components/TitleBar.tsx
+++ app/components/TitleBar.tsx
@@ -1,43 +1,84 @@
 import { Container, Icon, Menu } from 'semantic-ui-react';
 import React from 'react';
 import { Link } from 'react-router-dom';
-import {
-  CloseWindow,
-  MinimizeWindow,
-  ToggleMaximizeWindow
-} from '../utils/WindowManagement';
+import { remote } from 'electron';
 import styles from './TitleBar.scss';
 import routes from '../constants/routes.json';
 
-export default function TitleBar() {
-  return (
-    <Menu fixed="top" className={styles['title-bar']}>
-      <Container fluid>
-        <Menu.Item as="div" header>
-          WorkLog
-        </Menu.Item>
-        <Menu.Item as={Link} to={routes.HOME}>
-          Start
-        </Menu.Item>
-        <Menu.Item as={Link} to={routes.PROJECTS}>
-          Projects
-        </Menu.Item>
-        <Menu.Item as={Link} to={routes.COUNTER}>
-          Demo app page
-        </Menu.Item>
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export default class TitleBar extends React.Component<any, any> {
+  private static MinimizeWindow() {
+    remote.getCurrentWindow().minimize();
+  }
 
-        <Menu.Menu position="right">
-          <Menu.Item as="a" onClick={MinimizeWindow}>
-            <Icon name="window minimize" />
+  private static CloseWindow() {
+    remote.getCurrentWindow().close();
+  }
+
+  constructor(props: Readonly<{}>) {
+    super(props);
+    this.state = {
+      maximizeIcon: 'window maximize outline',
+      maximizeTitle: 'Maximize'
+    };
+    this.ToggleMaximizeWindow = this.ToggleMaximizeWindow.bind(this);
+  }
+
+  private ToggleMaximizeWindow() {
+    if (remote.getCurrentWindow().isMaximized()) {
+      remote.getCurrentWindow().unmaximize();
+      this.setState({
+        maximizeIcon: 'window maximize outline',
+        maximizeTitle: 'Maximize'
+      });
+    } else {
+      remote.getCurrentWindow().maximize();
+      this.setState({
+        maximizeIcon: 'window restore outline',
+        maximizeTitle: 'Restore'
+      });
+    }
+  }
+
+  render() {
+    /* eslint-disable react/destructuring-assignment */
+    return (
+      <Menu fixed="top" className={styles['title-bar']}>
+        <Container fluid>
+          <Menu.Item as="div" header>
+            WorkLog
           </Menu.Item>
-          <Menu.Item as="a" onClick={ToggleMaximizeWindow}>
-            <Icon name="window maximize" />
+          <Menu.Item as={Link} to={routes.HOME}>
+            Start
           </Menu.Item>
-          <Menu.Item as="a" onClick={CloseWindow}>
-            <Icon name="window close" />
+          <Menu.Item as={Link} to={routes.PROJECTS}>
+            Projects
           </Menu.Item>
-        </Menu.Menu>
-      </Container>
-    </Menu>
-  );
+          <Menu.Item as={Link} to={routes.COUNTER}>
+            Demo app page
+          </Menu.Item>
+
+          <Menu.Menu position="right">
+            <Menu.Item
+              as="a"
+              onClick={TitleBar.MinimizeWindow}
+              title="Minimize"
+            >
+              <Icon name="window minimize outline" />
+            </Menu.Item>
+            <Menu.Item
+              as="a"
+              onClick={this.ToggleMaximizeWindow}
+              title={this.state.maximizeTitle}
+            >
+              <Icon name={this.state.maximizeIcon} />
+            </Menu.Item>
+            <Menu.Item as="a" onClick={TitleBar.CloseWindow} title="Close">
+              <Icon name="window close outline" />
+            </Menu.Item>
+          </Menu.Menu>
+        </Container>
+      </Menu>
+    );
+  }
 }
